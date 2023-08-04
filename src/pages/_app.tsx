5c0aedd2e8ba7c87b7332af268549fb1669eca84
omotomiwa <tomiwa_chris@yahoo.com>
2023-08-04 23:22:36 +0100

    intial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
-import '@/styles/globals.css'
+import '@/styles/home.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import type { AppProps } from 'next/app'
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
